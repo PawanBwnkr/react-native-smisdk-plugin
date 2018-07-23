b6e68f63468205762bb5a1ba00c2cc264e306310
Pawan Bawankar <pawan.bawankar@datami.com>
2018-07-23 13:07:33 +0530

    initSponsoredData API removed from plugin and register SdStateListner added.

--- smisdk-plugin/src/main/java/com/datami/smisdk_plugin/SmiSdkReactModule.java
+++ smisdk-plugin/src/main/java/com/datami/smisdk_plugin/SmiSdkReactModule.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.util.Log;
 
 import com.datami.smi.Analytics;
-import com.datami.smi.SdStateChangeListener;
 import com.datami.smi.SmiResult;
 import com.datami.smi.SmiSdk;
 import com.facebook.react.bridge.Arguments;
@@ -19,11 +18,12 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SmiSdkReactModule extends ReactContextBaseJavaModule implements SdStateChangeListener {
+public class SmiSdkReactModule extends ReactContextBaseJavaModule {
 
     private Context mContext;
-    private String TAG = "SmiSdkReactModule";
-    private ReactApplicationContext mReactContext;
+    private static String TAG = "SmiSdkReactModule";
+    private static ReactApplicationContext mReactContext;
+    private static SmiResult sSmiResult = null;
 
     public SmiSdkReactModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -34,73 +34,6 @@ public class SmiSdkReactModule extends ReactContextBaseJavaModule implements SdS
     @Override
     public String getName() {
         return "SmiSdkReactModule";
-    }
-
-    @ReactMethod
-    public void initSponsoredData(String sdkKey, String userId, boolean showNotification, boolean showMessaging,
-                                  ReadableArray exclusionList, ReadableArray userTags){
-        Log.d(TAG, "initSponsoredData()");
-        int iconId = -1;
-        if(showNotification)
-        {
-            Log.d(TAG, "setting icon id");
-            iconId = mContext.getApplicationInfo().icon;
-
-        }
-
-        List<String> exclusionListJava = null;
-
-        if( (exclusionList!=null) && (exclusionList.size()>0))
-        {
-            exclusionListJava = new ArrayList<String>();
-            Log.i(TAG, "exclusion list length: "+exclusionList.size());
-            for(int i=0; i<exclusionList.size(); i++)
-            {
-                Log.i(TAG, "exclusion doman: "+exclusionList.getString(i));
-                exclusionListJava.add(exclusionList.getString(i));
-            }
-        }
-        else
-        {
-            Log.i(TAG, "exclusionList not available.");
-        }
-
-        List<String> userTagsListJava = null;
-
-        if( (userTags!=null) && (userTags.size()>0))
-        {
-            userTagsListJava = new ArrayList<String>();
-            Log.i(TAG, "userTags list length: "+userTags.size());
-            for(int i=0; i<userTags.size(); i++)
-            {
-                Log.i(TAG, "userTag: "+userTags.getString(i));
-                userTagsListJava.add(userTags.getString(i));
-            }
-        }
-        else
-        {
-            Log.i(TAG, "user Tags not available.");
-        }
-        Log.d(TAG, "icon id: " + iconId + ", showMessaging: " + showMessaging);
-        SmiSdk.initSponsoredData(sdkKey, mReactContext, userId, iconId, showMessaging, exclusionListJava, userTagsListJava);
-        SmiSdk.addSdStateChangeListener(this);
-    }
-
-    @Override
-    public void onChange(SmiResult smiResult) {
-        Log.d(TAG, "onChange:: " + smiResult.getSdState().name());
-        // Create map for params
-        WritableMap payload = Arguments.createMap();
-        // Put data to map
-        payload.putString("sd_state", smiResult.getSdState().name());
-        payload.putString("sd_reason", smiResult.getSdReason().name());
-        payload.putString("carrier_name", smiResult.getCarrierName());
-        payload.putString("client_ip", smiResult.getClientIp());
-
-        // Get EventEmitter from context and send event thanks to it
-        mReactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit("onSdStateChange", payload);
-        Log.d(TAG, "onChange --");
     }
 
     @ReactMethod
@@ -162,5 +95,40 @@ public class SmiSdkReactModule extends ReactContextBaseJavaModule implements SdS
         {
             Log.d(TAG, "userTags not available.");
         }
+    }
+
+    public static void setSmiResultToModule(SmiResult result){
+        Log.d(TAG, "setSmiResultToModule.");
+        sSmiResult = result;
+        // Get EventEmitter from context and send event to it
+        if(mReactContext!=null){
+            mReactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit("onSdStateChange", getMappings());
+        }
+    }
+
+    @ReactMethod
+    public void registerSdStateChangeListner(){
+        // Get EventEmitter from context and send event to it
+        Log.d(TAG, "registerSdStateChangeListner.");
+        if(mReactContext!=null){
+            mReactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit("onSdStateChange", getMappings());
+        }
+    }
+
+    private static WritableMap getMappings(){
+        // Create map for params
+        Log.d(TAG, "getMappings.");
+        WritableMap payload = Arguments.createMap();
+        // Put data to map
+        if(sSmiResult!=null){
+            payload.putString("sd_state", sSmiResult.getSdState().name());
+            payload.putString("sd_reason", sSmiResult.getSdReason().name());
+            payload.putString("carrier_name", sSmiResult.getCarrierName());
+            payload.putString("client_ip", sSmiResult.getClientIp());
+        }
+
+        return payload;
     }
 }
